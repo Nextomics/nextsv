ebc90501a0fbaa366a8fba5c52533387afaaaaa3
Li Fang <fangli0813@gmail.com>
2017-11-04 13:54:26 -0400

    update nextsv

--- setup-env.sh
+++
@@ -1,6 +0,0 @@
-
-export SWEETPATH=/home/lifang/software/nextsv/PBSuite_15.8.24
-export PYTHONPATH=$PYTHONPATH:$SWEETPATH
-export PATH=$PATH:$SWEETPATH/bin
-
-export LD_LIBRARY_PATH=/home/lifang/software/nextsv/blasr/lib:$LD_LIBRARY_PATH
